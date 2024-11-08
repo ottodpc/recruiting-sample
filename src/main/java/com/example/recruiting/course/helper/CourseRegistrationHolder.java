d86b525c5b79ad98080c06f0e98e6473f1bd8796
Juergen Windhaber <juergen.windhaber@gmail.com>
2024-11-08 13:16:50 +0100

    (polishing) makes code more concise

--- src/main/java/com/example/recruiting/course/helper/CourseRegistrationHolder.java
+++ src/main/java/com/example/recruiting/course/helper/CourseRegistrationHolder.java
@@ -2,35 +2,36 @@ package com.example.recruiting.course.helper;
 
 import com.example.recruiting.course.dto.CourseRegistrationDto;
 
+import java.time.LocalDate;
 import java.util.ArrayList;
 import java.util.List;
 
 import static com.example.recruiting.course.dto.CourseRegistrationDto.RegistrationStatus.CANCELED;
 import static com.example.recruiting.course.dto.CourseRegistrationDto.RegistrationStatus.ENROLLED;
-import static java.time.LocalDate.of;
+
 
 /**
  * @author juergen.windhaber
- *
+ * <p>
  * Holds all registrations which occured ofer time.
  */
 public class CourseRegistrationHolder {
 
-    public static final List<CourseRegistrationDto> REGISTRATIONS = new ArrayList<CourseRegistrationDto>() {{
-        add(new CourseRegistrationDto("Carl", ENROLLED, of(2020, 1 ,1)));
-        add(new CourseRegistrationDto("Carl", CANCELED ,of(2020, 1 ,2)));
-        add(new CourseRegistrationDto("Carl", ENROLLED, of(2020, 1 ,3)));
+    public static final List<CourseRegistrationDto> REGISTRATIONS = List.of(
+            new CourseRegistrationDto("Carl", ENROLLED, LocalDate.of(2020, 1, 1)),
+            new CourseRegistrationDto("Carl", CANCELED, LocalDate.of(2020, 1, 2)),
+            new CourseRegistrationDto("Carl", ENROLLED, LocalDate.of(2020, 1, 3)),
 
-        add(new CourseRegistrationDto("Mary", ENROLLED, of(2020, 1 ,1)));
-        add(new CourseRegistrationDto("Mary", CANCELED ,of(2020, 1 ,2)));
+            new CourseRegistrationDto("Mary", ENROLLED, LocalDate.of(2020, 1, 1)),
+            new CourseRegistrationDto("Mary", CANCELED, LocalDate.of(2020, 1, 2)),
 
-        add(new CourseRegistrationDto("Joey", CANCELED ,of(2020, 1 ,1)));
-        add(new CourseRegistrationDto("Joey", ENROLLED, of(2020, 1 ,2)));
+            new CourseRegistrationDto("Joey", CANCELED, LocalDate.of(2020, 1, 1)),
+            new CourseRegistrationDto("Joey", ENROLLED, LocalDate.of(2020, 1, 2)),
 
-        add(new CourseRegistrationDto("Bill", CANCELED ,of(2020, 1 ,1)));
-        add(new CourseRegistrationDto("Bill", CANCELED, of(2020, 1 ,2)));
+            new CourseRegistrationDto("Bill", CANCELED, LocalDate.of(2020, 1, 1)),
+            new CourseRegistrationDto("Bill", CANCELED, LocalDate.of(2020, 1, 2)),
 
-        add(new CourseRegistrationDto("Anne", ENROLLED ,of(2020, 1 ,1)));
-        add(new CourseRegistrationDto("Anne", ENROLLED, of(2020, 1 ,2)));
-    }};
+            new CourseRegistrationDto("Anne", ENROLLED, LocalDate.of(2020, 1, 1)),
+            new CourseRegistrationDto("Anne", ENROLLED, LocalDate.of(2020, 1, 2))
+    );
 }
